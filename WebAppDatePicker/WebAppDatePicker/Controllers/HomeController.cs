37786f8ef957d73db8cbdd98c75fb19f3889abcd
shivratankumar <tektuition@gmail.com>
2019-10-30 20:38:00 +0530

    Added WebAppDatePicker

---
+++ WebAppDatePicker/WebAppDatePicker/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppDatePicker.Models;
+
+namespace WebAppDatePicker.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            StudentModel objStudentModel = new StudentModel()
+            {
+                DateOfBirth = DateTime.Now
+            };
+            return View(objStudentModel);
+        }
+    }
+}
---
+++ WebAppDatePicker/WebAppDatePicker/Models/StudentModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAppDatePicker.Models
+{
+    public class StudentModel
+    {
+        public int StudentId { get; set; }
+
+        [Display(Name = "Student Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        [DisplayFormat(DataFormatString = "{0:dd-MMM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
